63d41e3a1c673e40e1e3c13830eb0b721c679d1c
sasha <aleksa_kolmakova@mail.ru>
2022-10-10 15:06:19 +0800

    add gitignor and firs task

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("!!!!!!!!!");
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            var str = Console.ReadLine();
+            int a= Convert.ToInt32(str);
+            if (a>=6 && a<=7)
+                Console.WriteLine($"{Environment.NewLine}Выходной!");
+            else
+                if (a<=5 && a>=1)
+                    Console.WriteLine($"{Environment.NewLine}Рабочий день!");
+                else 
+                    Console.WriteLine($"{Environment.NewLine}Вы ввели не верное число..");
+            Console.Write($"{Environment.NewLine}Нажмите любую кнопку...");
+            Console.ReadKey(true);
+        }
+    }
+}
